50f58b2357ccff5f0bb1fee79d1224a4842b5159
Kristofer Juhkam <kristofer.juhkam@voco.ee>
2026-04-08 11:05:51 +0300

    add file

--- strategy.py
+++ strategy.py
@@ -10,13 +10,38 @@ class OrdinaryDiscriminantStrategy(DiscriminantStrategy):
         return b * b - 4 * a * c
 
 
+class RealDiscriminantStrategy(DiscriminantStrategy):
+    def calculate_discriminant(self, a, b, c):
+        d = b * b - 4 * a * c
+        if d < 0:
+            return float('nan')
+        return d
+
+
 class QuadraticEquationSolver:
     def __init__(self, strategy):
         self.strategy = strategy
 
     def solve(self, a, b, c):
         d = self.strategy.calculate_discriminant(a, b, c)
+
+        if isinstance(d, float) and cmath.isnan(d):
+            return (complex(float('nan'), float('nan')),
+                    complex(float('nan'), float('nan')))
+
         sqrt_d = cmath.sqrt(d)
         x1 = (-b + sqrt_d) / (2 * a)
         x2 = (-b - sqrt_d) / (2 * a)
+
         return (x1, x2)
+
+
+# Näited
+solver1 = QuadraticEquationSolver(OrdinaryDiscriminantStrategy())
+print(solver1.solve(1, 10, 16))
+
+solver2 = QuadraticEquationSolver(RealDiscriminantStrategy())
+print(solver2.solve(1, 4, 5))
+
+solver3 = QuadraticEquationSolver(OrdinaryDiscriminantStrategy())
+print(solver3.solve(1, 4, 5))
