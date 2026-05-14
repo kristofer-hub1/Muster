c5df03e551bbb6c87a6968f5a0e69e756e09477a
kristofer-hub1 <kristofer.juhkam@voco.ee>
2026-05-14 10:28:17 +0300

    Commit

--- state.py
+++ state.py
@@ -1,6 +1,37 @@
 class CombinationLock:
     def __init__(self, combination):
-        self.combination = combination         
-        self.entered = []              
+        self.combination = combination
+        self.entered = []
         self.status = 'LOCKED'
-        
+
+    def enter_digit(self, digit):
+        self.entered.append(digit)
+
+        if self.entered != self.combination[:len(self.entered)]:
+            self.status = 'ERROR'
+
+        elif self.entered == self.combination:
+            self.status = 'OPEN'
+
+        else:
+            self.status = ''.join(map(str, self.entered))
+
+
+cl = CombinationLock([1, 2, 3, 4, 5])
+
+print(cl.status)
+
+cl.enter_digit(1)
+print(cl.status)
+
+cl.enter_digit(2)
+print(cl.status)
+
+cl.enter_digit(3)
+print(cl.status)
+
+cl.enter_digit(4)
+print(cl.status)
+
+cl.enter_digit(5)
+print(cl.status)
